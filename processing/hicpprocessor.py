3172693f63901787f6b944d706016d76bf5cc4a4
Cian Cockerill <ciancockerill@gmail.com>
2025-11-18 19:21:33 +0000

    Regex Matching for Categories

--- processing/hicpprocessor.py
+++ processing/hicpprocessor.py
@@ -7,6 +7,15 @@ import numpy as np
 def main():
     df = pd.read_csv('data/original/hicp.csv')
 
+    # Drop Statistic Label (Static String) & UNIT (Static String)
+    df = df.drop(columns=['Statistic Label', 'UNIT'])
+
+    # Split Existing Commodity Group Column
+    cg_series = df['Commodity Group']
+    cg_series_code = cg_series.str.extract('\(COICOP\s(.*)\)') # Sewerage collection (COICOP 04.4.3) -> 04.4.3
+    cg_series_name = cg_series.str.extract('(.*)(?:\s\(.*\))') # Sewerage collection (COICOP 04.4.3) -> Sewerage collection
+
+    print(cg_series_name)
 
 if __name__ == '__main__':
     main()

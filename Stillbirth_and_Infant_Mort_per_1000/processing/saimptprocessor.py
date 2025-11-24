917836ad816c40970fe17f6c8e735f54cbca664f
Cian Cockerill <ciancockerill@gmail.com>
2025-11-24 00:40:18 +0000

    New data & start of processing

---
+++ Stillbirth_and_Infant_Mort_per_1000/processing/saimptprocessor.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def main():
+    df = pd.read_csv('Stillbirth_and_Infant_Mort_per_1000/data/original/saimpt.csv')
+    print(df)
+
+
+if __name__ == '__main__':
+    main()
+
